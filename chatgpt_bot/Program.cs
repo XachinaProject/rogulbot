6ca993ddfbd8b35617bced8246bb8a600aa1d5aa
Alexey <gamesense99@mail.ru>
2023-02-12 05:47:25 +0300

    commit
    commit

---
+++ chatgpt_bot/Program.cs
@@ -0,0 +1,57 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+using OpenAI_API.Models;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+OpenAIAPI api = new OpenAIAPI("api-from-openai"); // shorthand
+var botClient = new TelegramBotClient("telegram-bot-api");
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+    var chatId = message.Chat.Id;
+    var response = await api.Completions.CreateCompletionAsync(new CompletionRequest(messageText, Model.DavinciText, 400, 0.5, presencePenalty: 0.1, frequencyPenalty: 0.1)); //можете ставить 200 токенов или 300, потому что 400 вроде норм, а вроде прям топ хз, тестируйте -_-
+    Message sentMessage = await botClient.SendTextMessageAsync(chatId: chatId, text: response.ToString(), cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
